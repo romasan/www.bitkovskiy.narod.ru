e8fc7fdc1e94d77813f39e4d4d4822668dccd80e
roman <kotapesic@gmail.com>
2016-12-07 09:16:26 +0000

    add files

---
+++ README.md
@@ -0,0 +1,1 @@
+# www.bitkovskiy.narod.ru
---
+++ doc/script.js
@@ -0,0 +1,9 @@
+window.status = "(сайт находится на стадии разработки) Разработка: Метелёва Наталья, Бауэр Роман";
+function menu(i,j){
+i.style.display = "block";
+j.style.display = "none";
+}
+function new_w(i){
+i = i + ".htm";
+window.open(i,"","resizable=1, width=640, height=480");
+}
